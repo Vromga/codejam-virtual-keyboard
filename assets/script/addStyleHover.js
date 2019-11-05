35d79d76c600e56936efa23ce91cca7ddd3c36db
Mikita Maiseichek <Doktornikita@hotmail.com>
2019-11-05 23:02:03 +0300

    fix: button Ctrl, win, space

--- assets/script/addStyleHover.js
+++ assets/script/addStyleHover.js
@@ -3,7 +3,7 @@ import styleSet from "./styleSetting.js";
 function addHover() {
     const collectionKey = document.querySelectorAll(`.${styleSet.key}`);
     [...collectionKey].forEach(key => {
-        if (event.key === key.innerText) {
+        if (event.key === key.innerText || event.key === key.getAttribute('data')) {
             key.classList.add(styleSet.hover);
         }
     });
