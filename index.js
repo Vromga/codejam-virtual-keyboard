07ce2731c7539eea05b3a37659b41a0c0bb18a5a
Mikita Maiseichek <Doktornikita@hotmail.com>
2019-11-05 17:32:33 +0300

    feat: add createElement function

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-//import keyboardLayout from "./assets/script/keysSetting.js";
-// import styleSet from "./assets/script/styleSetting.js";
+import keyboardLayout from "./assets/script/keysSetting.js";
+import styleSet from "./assets/script/styleSetting.js";
+
+'use strict';
+// add keyLayout
+let ruLayout = keyboardLayout.ru.flat();
+let enLayout = keyboardLayout.en.flat();
+
+function createElement(elem, insert, style) {
+    let parent = document.querySelector(insert);
+    let child = document.createElement(elem);
+    child.classList.add(style);
+    parent.insertAdjacentElement('afterbegin', child);
+}
+
+createElement('input', 'body', styleSet.input);
+createElement('div', 'body', styleSet.body);
