550a68ecc913cb86527be2171f6befdf6590bca7
Mikita Maiseichek <Doktornikita@hotmail.com>
2019-11-05 17:27:46 +0300

    feat: add styleSetting module

---
+++ assets/script/styleSetting.js
@@ -0,0 +1,20 @@
+const styleSet = {
+    body: 'body_keyboard',
+    key: 'body_keyboard--key',
+    backspace: 'backspace',
+    tab: 'tab',
+    caps: 'caps',
+    enter: 'enter',
+    shift: 'shift',
+    ctrl: 'ctrl',
+    space: 'space',
+    upDown: 'up_down',
+    arrowUp: 'up_down--up',
+    arrowDown: 'up_down--down',
+    func: 'func',
+    input: 'text',
+    hidden: 'hidden',
+    hover: 'hover'
+};
+
+export default styleSet;
--- index.js
+++ index.js
@@ -1,1 +1,2 @@
 //import keyboardLayout from "./assets/script/keysSetting.js";
+// import styleSet from "./assets/script/styleSetting.js";
