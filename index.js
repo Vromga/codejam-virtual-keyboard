db9cd889d43ceb5620d1931130f27657cf2f9348
Mikita Maiseichek <Doktornikita@hotmail.com>
2019-11-06 16:52:14 +0300

    fix: gitignore update and eslint fix

--- index.js
+++ index.js
@@ -2,60 +2,55 @@ import styleSet from './assets/script/styleSetting.js';
 import createElement from './assets/script/createElelement.js';
 import specialKeyStyle from './assets/script/specialKeyStyle.js';
 import addHover from './assets/script/addStyleHover.js';
-import removeHover from "./assets/script/removeStyleHover.js";
+import removeHover from './assets/script/removeStyleHover.js';
 import removeKey from './assets/script/removeKey.js';
 import changeLang from './assets/script/changeLang.js';
 
 
-
 function keyboardInit() {
-    let out = '';
+  let out = '';
 
-    createElement('input', 'body', styleSet.input);
-    createElement('div', 'body', styleSet.body);
+  createElement('input', 'body', styleSet.input);
+  createElement('div', 'body', styleSet.body);
 
-    const input = document.querySelector(`.${styleSet.input}`);
-    input.setAttribute('placeholder', 'Welcome to virtual keyboard...');
+  const input = document.querySelector(`.${styleSet.input}`);
+  input.setAttribute('placeholder', 'Welcome to virtual keyboard...');
+  const bodyDocument = document.querySelector('body');
 
-    const keyboard = document.querySelector(`.${styleSet.body}`);
-    const bodyDocument = document.querySelector('body');
-
-
-    changeLang();
-    specialKeyStyle();
-
-    bodyDocument.addEventListener('keydown', (event) => {
-        addHover();
-        letter(event);
-        if (event.ctrlKey) { // change lang
-            removeKey();
-            changeLang();
-            specialKeyStyle();
-            addHover();
-        }
-    });
-
-    bodyDocument.addEventListener('keyup', () => {
-        removeHover();
-    });
-
-    bodyDocument.addEventListener('click', (event) => {
-        console.dir(event);
-        if(event.target.textContent.length === 1){
-            out += event.target.textContent;
-            input.value = '';
-            input.value = out;
-        }
-    });
-
-    function letter(event) {
-        if(event.key.length === 1){
-            let letter = document.querySelector(`.${styleSet.key}[data='${event.code}']`);
-            out += letter.textContent;
-            input.value = '';
-            input.value = out;
-        }
+  function letter(event) {
+    if (event.key.length === 1) {
+      const letterData = document.querySelector(`.${styleSet.key}[data='${event.code}']`);
+      out += letterData.textContent;
+      input.value = '';
+      input.value = out;
     }
+  }
+
+  changeLang();
+  specialKeyStyle();
+
+  bodyDocument.addEventListener('keydown', (event) => {
+    addHover();
+    letter(event);
+    if (event.ctrlKey) { // change lang
+      removeKey();
+      changeLang();
+      specialKeyStyle();
+      addHover();
+    }
+  });
+
+  bodyDocument.addEventListener('keyup', () => {
+    removeHover();
+  });
+
+  bodyDocument.addEventListener('click', (event) => {
+    if (event.target.textContent.length === 1) {
+      out += event.target.textContent;
+      input.value = '';
+      input.value = out;
+    }
+  });
 }
 
 keyboardInit();
